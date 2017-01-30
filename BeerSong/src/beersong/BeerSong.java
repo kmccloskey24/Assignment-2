db368753670c3471480e7e7958ab75e2775ab67d
kmccloskey24 <kpmcclos@oakland.edu>
2017-01-30 14:25:26 -0500

    if
    if started for beer number amount

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -26,6 +26,9 @@ public class BeerSong {
             System.out.println(beerNum + " " + word + " of beer on the wall, " + beerNum + " " + word + " of beer");
             beerNum = beerNum - 1;
 
+            if (beerNum > 0) {
+                System.out.println("Take one down, pass it round " + beerNum + " " + word + " of beer");
+            }
           }
          
     
