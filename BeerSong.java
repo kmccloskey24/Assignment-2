58e1632544f843ec800509333c9d15164a09be3e
kmccloskey24 <kpmcclos@oakland.edu>
2017-01-30 14:31:18 -0500

    Title
    title for assignment 2

--- BeerSong.java
+++ BeerSong.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Keenan McCloskey
+ * CIT 337 Assignment #2
+ * 1-30-17
  */
 package beersong;
 
